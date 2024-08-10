8fa0139d03756778457a1d898bb3595d0d0a4e83
Ryndina Olena <110286532+RyndinaOlena@users.noreply.github.com>
2024-08-10 20:16:43 +0300

    layout

---
+++ src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image';
+import { nunito } from '@/app/ui/fonts';
+import NavLinks from '@/app/ui/dashboard/nav-links';
+
+import Link from "next/link";
+const Head = () => {
+    return (
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center align-center space-x-2 p-2 grow'>
+                <Image
+                    src={`/logo.png`}
+                    alt="logo"
+                    width={55}
+                    height={55}
+
+                />
+                <h1 className={`text-black text-4xl font-extrabold text-slate-800  ${nunito.className}`}>Toloka</h1>
+
+            </div>
+            <div className='flex-1 text-center '  >
+                <NavLinks />
+
+            </div>
+            <div className='flex-1 text-right'>
+                <Link href='/' className={`text-white pb-2.5 pt-2.5 pl-9 pr-9  bg-lime-700 rounded-full ${nunito.className}`}>УВІЙТИ</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Head
---
+++ src/app/dashboard/header/blog/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blog = () => {
+    return (
+        <div>
+            <h1 className='text-blue-600'>Blog</h1>
+        </div>
+    )
+}
+
+export default Blog
